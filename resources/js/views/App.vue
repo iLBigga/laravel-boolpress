3c18979f25fb4d4db5baa37858e534514286cec9
iLBigga <Giannanti1996@gmail.com>
2022-11-01 00:21:08 +0100

    Creato Scaffolding

---
+++ resources/js/views/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>{{ title }}</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                title: 'Bentornato'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/admin', function () {
     return view('welcome');
 });
 
@@ -26,6 +26,9 @@ Route::middleware('auth')
     ->group(function() {
 
         Route::get('/home', 'HomeController@index')->name('home');
-
         Route::resource('posts', 'PostController');
-    });
+});
+
+Route::get('{any?}', function (){ 
+    return view('guest.home');
+})->where('any', '.*');
